87289fc7475a153bc7ffa64a463c5711c97baa90
Maikel Chandika <mkdika@gmail.com>
2020-04-30 21:02:43 +0700

    Add fibs demo and variable types

---
+++ src/03_variable-types.cr
@@ -0,0 +1,31 @@
+name = "Diamond"
+element = 'C'
+hardness = 10
+
+puts typeof(name)
+puts typeof(element)
+puts typeof(hardness)
+
+# assign multiple value
+a,b,c = "A",2,3.5
+
+# assign multiple statement with semi colon
+x = "Diamond"; y = 'C'; z = 10;
+
+# swap 2 variable
+n = 41
+m = 42
+n, m = m, n
+puts n
+puts m
+
+# variable can reassign/ mutable
+namae = "maikel"
+puts namae
+namae = "chandika"
+puts namae
+
+# constanta is will all upcase
+FAMILY_NAME = "chandika"
+puts FAMILY_NAME
+# FAMILY_NAME = "chaniago" # reassign will cause error compile time
