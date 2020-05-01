19ebf6931d9baa4a2329d43a0c95c4160cac17e3
Maikel Chandika <mkdika@gmail.com>
2020-05-01 10:24:41 +0700

    Update hello world example, add unicode color & emoji demo

--- src/01_helloworld.cr
+++ src/01_helloworld.cr
@@ -2,3 +2,8 @@ puts "Hello World"
 
 # or event shorter, but will print with quotes
 p "Hello World shorter"
+
+# print unicode char
+emoji = "\u{1f431}\u{1f436}\u{1f98a}"
+puts "\e[31mDemo print unicode emoji with red color text:\e[0m #{emoji}"
+puts "end!"
